3fa90a0c67189658926915eab8302232da8cb732
Helmut Steiner <csae1152@uibk.ac.at>
2015-10-11 16:34:44 +0200

    Update HttpServer.java

--- HttpServer.java
+++ HttpServer.java
@@ -1,65 +1,74 @@
 package com.pointwise.httpserver;
 
+import com.opower.finagle.resteasy.server.ResteasyServiceBuilder;
 import com.twitter.finagle.Service;
+import com.twitter.finagle.builder.Server;
 import com.twitter.finagle.builder.ServerBuilder;
-import com.twitter.finagle.http.*;
+import com.twitter.finagle.http.Http;
 import com.twitter.finagle.http.HttpMuxer;
 import com.twitter.util.Future;
+import com.twitter.util.FutureTransformer;
 import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.handler.codec.http.*;
-import org.jboss.netty.util.CharsetUtil;
+
 import java.net.InetSocketAddress;
 
-public class HttpServer {
-    public static int Calculate(String query) throws NumberFormatException {
-        int sum = 0;
-        System.out.println(query + " query");
-        for (String ints : query.split(",")) {
-            System.out.println(ints);
-            try {
-                sum = sum + Integer.parseInt(ints.toString());
-            } catch (NumberFormatException e) {
-                System.out.println(e);
-                return -1;
+/**
+ * Created by lala on 14-1-29.
+ */
+public class HttpServer extends Service<HttpRequest, HttpResponse> {
+    
+    
+    private Future<String> getContentAsync(HttpRequest request) {
+        // asynchronously gets content, possibly by submitting
+        // a function to a FuturePool
+        return Future.value("content");
+    }
+
+    public Future<HttpResponse> apply(HttpRequest request) {
+
+        Future<String> contentFuture = getContentAsync(request);
+        return contentFuture.transformedBy(new FutureTransformer<String, HttpResponse>() {
+            @Override
+            public HttpResponse map(String content) {
+                HttpResponse httpResponse =
+                        new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+                httpResponse.setContent(ChannelBuffers.wrappedBuffer(content.getBytes()));
+                return httpResponse;
             }
-        }
-        return sum;
+
+            @Override
+            public HttpResponse handle(Throwable throwable) {
+                HttpResponse httpResponse =
+                        new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.SERVICE_UNAVAILABLE);
+                httpResponse.setContent(ChannelBuffers.wrappedBuffer(throwable.toString().getBytes()));
+                return httpResponse;
+            }
+        });
     }
 
-    public static int RemoteMethod(HttpRequest request) throws StringIndexOutOfBoundsException {
-        try {
-            String xx = request.getUri();
-            xx = xx.substring(2);
-            return Calculate(xx);
-        } catch (StringIndexOutOfBoundsException s) {
-            System.out.println(s);
-            return -1;
-        }
+    public static void main(String[] args) {
+        HttpMuxer muxService = new HttpMuxer().withHandler("/foo/bar", new HttpServer());
+            
+        Service service = ResteasyServiceBuilder.get()
+                .withEndpoint(new RoutingService() {
 
+            @Override
+            public String getGreeting() {
+              return "Hello, World!";
+            }
+        } )
+           
+      
+                .build();
+        
+        ServerBuilder builder = ServerBuilder.get()
+                .name("ExampleServer")
+                .codec(Http.get())
+                .bindTo(new InetSocketAddress("localhost", 8000));
 
+        Server server = ServerBuilder.safeBuild(service, builder);
+        
+        System.out.println("Server start...");
     }
-
-
-        public static void main(String[] args) {
-
-            Service<HttpRequest, HttpResponse> service = new Service<HttpRequest, HttpResponse>() {
-
-                public Future<HttpResponse> apply(HttpRequest request) {
-
-                    String result = Integer.toString(RemoteMethod(request));
-                    HttpResponse res = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
-                    if (result.equals("-1")) {
-                        res.setContent(ChannelBuffers.copiedBuffer("Enter Correct URL, Correct Format is http://localhost:10000/?int1,int2,int3,..", CharsetUtil.UTF_8));
-                       // res.setHeader(HttpHeaders.Names.CONTENT_TYPE, "text/bold; charset=UTF-8");
-                        
-                    } else {
-                        res.setContent(ChannelBuffers.copiedBuffer("Your Sum is: " + result, CharsetUtil.UTF_8));
-                        //res.setHeader(HttpHeaders.Names.CONTENT_TYPE, "text/bold; charset=UTF-8");
-                    }
-                    return Future.<HttpResponse>value(res);
-                }
-            };
-
-            ServerBuilder.safeBuild(service, ServerBuilder.get().codec(Http.get()).name("HttpServer").bindTo(new InetSocketAddress("localhost", 8000)));
-        }
-    }
+}
