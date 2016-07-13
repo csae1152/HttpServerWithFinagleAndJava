8d17027483dc09e55f1ea5dcf5ea8bcfa55dffa8
Helmut Steiner <csae1152@uibk.ac.at>
2016-07-13 15:20:24 +0200

    Update HttpServer.java

--- HttpServer.java
+++ HttpServer.java
@@ -90,7 +90,7 @@ public class HttpServer extends Service<HttpRequest, HttpResponse> implements Ru
         ServerBuilder builder = ServerBuilder.get()
                 .name("HttpServer")
                 .codec(Http.get())
-                .bindTo(new InetSocketAddress("localhost", 9080));
+                .bindTo(new InetSocketAddress("localhost", 8080));
 
         public void run() {
             Server server = ServerBuilder.safeBuild(service, builder);
